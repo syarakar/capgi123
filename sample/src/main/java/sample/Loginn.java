86089320db054ad8fd69e78aa67f59d1cf840694
syarakar <syarakar@DIN78000389.corp.capgemini.com>
2019-07-04 15:19:05 +0530

    Login feature included with loginn class

---
+++ sample/src/main/java/sample/Loginn.java
@@ -0,0 +1,23 @@
+package sample;
+
+public class Loginn {
+	private String username;
+	private String password;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "Loginn [username=" + username + ", password=" + password + "]";
+	}
+
+}
